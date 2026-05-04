f90437a52ae831f129131e2b07b9cb0730c46f3f
jaypventures <jaypventures@icloud.com>
2026-05-04 01:51:17 -0600

    Refactor code structure for improved readability and maintainability

--- .next/dev/server/pages/_document.js
+++
@@ -1,5 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_document.js")
-R.c("server/chunks/ssr/12d~_0zozdjo._.js")
-R.c("server/chunks/ssr/[externals]__0.o~_~l._.js")
-R.m("[project]/projects/jayventures-labs/node_modules/next/document.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/projects/jayventures-labs/node_modules/next/document.js [ssr] (ecmascript)").exports
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"e/VQkL+jICqowqqVk45nVSFdQhatldm3LY/NFQGRFk8=\"\n}"
